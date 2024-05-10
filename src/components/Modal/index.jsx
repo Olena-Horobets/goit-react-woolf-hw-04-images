70736bf420e1ef15fa0eb63404de7d5a86a38709
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-05-09 21:31:09 -0230

    modal changed to hooks

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,59 +1,50 @@
 import s from './Modal.module.css';
 
 import { createPortal } from 'react-dom';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { ReactComponent as ReactSprite } from 'images/svg/sprite.svg';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.onEscapePress);
-  }
+function Modal({ alt, src, onModalClose }) {
+  useEffect(() => {
+    window.addEventListener('keydown', onEscapePress);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onEscapePress);
-  }
+    return () => {
+      window.removeEventListener('keydown', onEscapePress);
+    };
+  }, []);
 
-  onEscapePress = e => {
-    return e.code !== 'Escape' ? null : this.props.onModalClose();
+  const onEscapePress = e => {
+    return e.code !== 'Escape' ? null : onModalClose();
   };
 
-  onBackdropClick = e => {
-    return e.target !== e.currentTarget ? null : this.props.onModalClose();
+  const onBackdropClick = e => {
+    return e.target !== e.currentTarget ? null : onModalClose();
   };
 
-  render() {
-    return createPortal(
-      <div className={s.lightbox}>
-        <ReactSprite />
-        <div
-          className={s.lightbox__overlay}
-          onClick={this.onBackdropClick}
-        ></div>
-        <div className={s.lightbox__content}>
-          <img
-            className={s.lightbox__image}
-            src={this.props.src}
-            alt={this.props.alt}
-          />
-          <div className={s.lightbox__info}>
-            <button
-              type="button"
-              className={s.lightbox__buttonClose}
-              onClick={this.props.onModalClose}
-              aria-label="close Modal Window"
-            >
-              <svg className={s.icon} width="32" height="32">
-                <use href="#icon-close"></use>
-              </svg>
-            </button>
-          </div>
+  return createPortal(
+    <div className={s.lightbox}>
+      <ReactSprite />
+      <div className={s.lightbox__overlay} onClick={onBackdropClick}></div>
+      <div className={s.lightbox__content}>
+        <img className={s.lightbox__image} src={src} alt={alt} />
+        <div className={s.lightbox__info}>
+          <button
+            type="button"
+            className={s.lightbox__buttonClose}
+            onClick={onModalClose}
+            aria-label="close Modal Window"
+          >
+            <svg className={s.icon} width="32" height="32">
+              <use href="#icon-close"></use>
+            </svg>
+          </button>
         </div>
-      </div>,
-      modalRoot
-    );
-  }
+      </div>
+    </div>,
+    modalRoot
+  );
 }
 
 export default Modal;
